29466dbb4502e797b380148f7fe06e9cf487d3af
Rupert Minnett <rminnett@Gmail.com>
2020-11-16 12:53:46 -0500

    Add content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+import { getGithubPreviewProps, parseJson } from 'next-tinacms-github'
+import { GetStaticProps } from 'next'
+
+export default function Home({ file }) {
+	const data = file.data
   return (
     <div className={styles.container}>
       <Head>
@@ -62,4 +66,31 @@ export default function Home() {
       </footer>
     </div>
   )
+}
+
+/**
+ * Fetch data with getStaticProps based on 'preview' mode
+ */
+export const getStaticProps: GetStaticProps = async function({
+	preview,
+	previewData,
+}) {
+	if (preview) {
+		return getGithubPreviewProps({
+			...previewData,
+			fileRelativePath: 'content/home.json',
+			parse: parseJson,
+		})
+	}
+	return {
+		props: {
+			sourceProvider: null,
+			error: null,
+			preview: false,
+			file: {
+				fileRelativePath: 'content/home.json',
+				data: (await import('../content/home.json')).default,
+			},
+		},
+	}
 }
