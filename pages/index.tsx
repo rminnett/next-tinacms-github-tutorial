5e69440b960101d34d8afd3d3c8e9be1f8f1ddba
Rupert Minnett <rminnett@Gmail.com>
2020-11-16 13:06:14 -0500

    Make data editable

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,18 @@ import styles from '../styles/Home.module.css'
 
 import { getGithubPreviewProps, parseJson } from 'next-tinacms-github'
 import { GetStaticProps } from 'next'
+import { usePlugin } from 'tinacms'
+import { useGithubJsonForm, useGithubToolbarPlugins } from 'react-tinacms-github'
 
 export default function Home({ file }) {
-	const data = file.data
+	const formOptions = {
+	  label: 'Home Page',
+	  fields: [{ name: 'title', component: 'text' }],
+	}
+	
+	const [data, form] = useGithubJsonForm(file, formOptions)
+	usePlugin(form)
+	useGithubToolbarPlugins()
   return (
     <div className={styles.container}>
       <Head>
@@ -16,6 +25,7 @@ export default function Home({ file }) {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
+					{data.title}
         </h1>
 
         <p className={styles.description}>
